d0f425cff2ff79dfe982d191387d74700ae9add1
Christopher Zuniga <c.zuniga21@gmail.com>
2020-09-27 16:09:58 -0600

    Structure :  adding layers

---
+++ AA_ChallengeBackend/Utils/DInjection/ClassType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.DInjection
+{
+    public class ClassType<i, t> : IClassType
+    {
+        public ClassType()
+        {
+
+        }
+        public Type GetInterface()
+        {
+            return typeof(i);
+        }
+        public Type GetClass()
+        {
+            return typeof(t);
+        }
+    }
+}
