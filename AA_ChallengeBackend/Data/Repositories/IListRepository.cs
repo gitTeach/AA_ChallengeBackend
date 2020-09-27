9e57d76b618ccd27d9a15a556a915ae8ce6c0788
Christopher Zuniga <c.zuniga21@gmail.com>
2020-09-27 17:25:49 -0600

    Working on Repositories

---
+++ AA_ChallengeBackend/Data/Repositories/IListRepository.cs
@@ -0,0 +1,18 @@
+﻿using Data.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories
+{
+    public interface IListRepository
+    {
+        IEnumerable<TList> GetLists(string userId);
+        TList GetList(string userId, int idList);
+        void AddList(string userId, TList list);
+        void UpdateList(TList list);
+        void DeleteList(TList list);
+        bool Exist(int idList);
+        bool Save();
+    }
+}
---
+++ AA_ChallengeBackend/Data/Repositories/ListRepository.cs
@@ -0,0 +1,45 @@
+﻿using Data.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories
+{
+    public class ListRepository : IListRepository
+    {
+        public void AddList(string userId, TList list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteList(TList list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist(int idList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TList GetList(string userId, int idList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TList> GetLists(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateList(TList list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
