f003d22463b2c07fb636f372c4573bc32388cab4
Christopher Zuniga <c.zuniga21@gmail.com>
2020-09-27 19:15:20 -0600

    Working on contollers and implementing automapper.

---
+++ AA_ChallengeBackend/WebAPI/Controllers/ListController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListController : BaseAPIController
+    {
+        public IListService _listService;
+        private readonly IMapper _mapper;
+        public ListController(IListService listService, IMapper mapper)
+        {   
+            _listService = listService ?? throw new ArgumentNullException(nameof(listService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        [ActionName("getAll")]
+        public ActionResult<IEnumerable<ListDTO>> GetListForUser(string userId)
+        {
+            try
+            {
+                var data = _listService.GetLists(userId);
+                return Ok(_mapper.Map<IEnumerable<ListDTO>>(data));
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ AA_ChallengeBackend/WebAPI/Profiles/ListProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace WebAPI.Profiles
+{
+    public class ListProfile : Profile
+    {
+        public ListProfile()
+        {
+            CreateMap<Data.DbModels.TList, Domain.Models.ListDTO>();
+
+            //var config = new MapperConfiguration(cfg => {
+            //    cfg.CreateMap<Data.DbModels.TList, Domain.Models.ListDTO>();
+            //});
+
+            //var mapper = new Mapper(config);
+        }
+    }
+}
